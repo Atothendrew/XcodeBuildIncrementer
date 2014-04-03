cf6d6fcc9f14c3cc3b490900fdbdd082842af1ba
Andrew Williamson <atothendrew24@mac.com>
2014-04-03 15:09:28 -0700

    New method

--- XcodeBuildIncrementer/XcodeBuildIncrementer.py
+++ XcodeBuildIncrementer/XcodeBuildIncrementer.py
@@ -8,80 +8,97 @@ try:
     BUILD_CONFIGURATION = os.environ['CONFIGURATION']
     BUILD_CONFIGURATION_DIR = os.environ['CONFIGURATION_BUILD_DIR']
     IS_RELEASE = BUILD_CONFIGURATION == "Release"
-    IS_ARCHIVE = "ArchiveIntermediates" in BUILD_CONFIGURATION_DIR and IS_RELEASE
+    IS_ARCHIVE = "ArchiveIntermediates" in BUILD_CONFIGURATION_DIR
+
 except:
     print "Could not find the required environment variables: PROJECT_DIR, INFOPLIST_FILE\nIf this is being run in Xcode, then this is an error! Trying command line args..."
     # TODO: Add command line option
 
-def get_new_bundle_version(CFBundleShortVersionString, CFBundleVersion):
+
+def get_new_version(version, IS_ARCHIVE, IS_RELEASE):
     """
     Get a new bundle version using
     :param CFBundleShortVersionString: str - The current version of the app
     :param CFBundleVersion: - The current bundle version of the app
 
-    >>> print get_new_bundle_version(CFBundleShortVersionString="1.3.2",CFBundleVersion="4325")
-    4326
+    >>> print get_new_version(version="1.3.2.5", IS_ARCHIVE=False, IS_RELEASE=False)
+    1.3.2.5
 
-    >>> print get_new_bundle_version(CFBundleShortVersionString="5",CFBundleVersion="7000")
-    7001
+    >>> print get_new_version(version="1.3.2.5", IS_ARCHIVE=True, IS_RELEASE=False)
+    1.3.2
 
-    >>> print get_new_bundle_version(CFBundleShortVersionString="1",CFBundleVersion="1.1")
-    1.2
+    >>> print get_new_version(version="1.3.2.5", IS_ARCHIVE=False, IS_RELEASE=True)
+    1.3.2.6
 
-    >>> print get_new_bundle_version(CFBundleShortVersionString="1.3.2",CFBundleVersion="1.3.2.1")
-    1.3.2.2
+    >>> print get_new_version(version="1.3.2", IS_ARCHIVE=False, IS_RELEASE=False)
+    1.3.2
 
-    >>> print get_new_bundle_version(CFBundleShortVersionString="1.3.2.4",CFBundleVersion="1.3.2.5")
-    1.3.2.4.1
+    >>> print get_new_version(version="1.3.2", IS_ARCHIVE=True, IS_RELEASE=False)
+    1.3
 
-    >>> print get_new_bundle_version(CFBundleShortVersionString="1.2",CFBundleVersion="1.2.1")
-    1.2.2
+    >>> print get_new_version(version="1.3.2", IS_ARCHIVE=False, IS_RELEASE=True)
+    1.3.3
 
-    >>> print get_new_bundle_version(CFBundleShortVersionString="1.3.1",CFBundleVersion="1.3.1")
-    1.3.1.1
+    >>> print get_new_version(version="1.3", IS_ARCHIVE=False, IS_RELEASE=False)
+    1.3
 
-    >>> print get_new_bundle_version(CFBundleShortVersionString="1.3.2",CFBundleVersion="1.2.2.1")
-    1.3.2.1
+    >>> print get_new_version(version="1.3", IS_ARCHIVE=True, IS_RELEASE=False)
+    1.0
+
+    >>> print get_new_version(version="1.3", IS_ARCHIVE=False, IS_RELEASE=True)
+    1.4
 
     """
 
+    if not IS_ARCHIVE and not IS_RELEASE:
+        return version
+
+    version_split = version.split('.')
+    version_split_sigfigs = len(version_split)
+
+    # ARCHIVE
     if IS_ARCHIVE:
-        return CFBundleShortVersionString
-    elif IS_RELEASE:
+        if version_split_sigfigs == 2:
+            version_split[1] = str(0)
+        else:
+            del version_split[-1]
+        version = ".".join(version_split)
+        return version
 
-        if "." not in CFBundleVersion:
-            return str(int(CFBundleVersion)+1)
+    # Release
+    else:
+        version_split[version_split_sigfigs-1] = str(int(version_split[version_split_sigfigs-1]) + 1)
+        return ".".join(version_split)
 
-        version_string_split = CFBundleShortVersionString.split('.')
-        old_bundle_version_split = CFBundleVersion.split('.')
+def get_new_build(old_version, new_version, build):
+    """
+    Get a new bundle version using
+    :param CFBundleShortVersionString: str - The current version of the app
+    :param CFBundleVersion: - The current bundle version of the app
 
-        version_string_sigfigs = len(version_string_split)
-        old_bundle_version_sigfigs = len(old_bundle_version_split)
+    >>> print get_new_build(old_version="1.3.2", new_version="1.3.2", build="4325")
+    4326
 
-        if version_string_sigfigs == old_bundle_version_sigfigs-1:
+    >>> print get_new_build(old_version="1.3.2", new_version="1.3.3", build="4325")
+    1
 
-            def sig_figs_match(version_string_split, old_bundle_version_split):
-                new_bundle_list_split = list(old_bundle_version_split)
-                del new_bundle_list_split[-1]
-                ziped_list = zip(version_string_split,  new_bundle_list_split)
-                true_false = all(i == y for i, y in ziped_list)
-                return true_false
+    """
 
-            sig_figs_match = sig_figs_match(version_string_split, old_bundle_version_split)
+    # Version did not change, increment the current build number
+    if old_version == new_version:
+        return str(int(build) + 1)
 
-            if sig_figs_match:
-                old_bundle_version_split[old_bundle_version_sigfigs-1] = str(int(old_bundle_version_split[old_bundle_version_sigfigs-1]) + 1)
-                return ".".join(old_bundle_version_split)
+    # Version changed, start over at 1
+    else:
+        return str(1)
 
-
-        return CFBundleShortVersionString + ".1"
-
-    return CFBundleVersion
 
 def run_and_change_build():
     plist_filename = "{project_dir}/{infoplist_file}".format(project_dir=PROJECT_DIR, infoplist_file=INFOPLIST_FILE)
     plist = plistlib.readPlist(plist_filename)
-    new_bundle_version = get_new_bundle_version(plist["CFBundleShortVersionString"], plist["CFBundleVersion"])
+    new_short_version = get_new_version(version=plist["CFBundleShortVersionString"], IS_ARCHIVE=IS_ARCHIVE, IS_RELEASE=IS_RELEASE)
+    new_bundle_version = get_new_build(old_version=plist["CFBundleShortVersionString"], new_version=new_short_version, build=plist["CFBundleVersion"])
+    plist["CFBundleShortVersionString"] = new_short_version
     plist["CFBundleVersion"] = new_bundle_version
     plistlib.writePlist(plist, plist_filename)
 
